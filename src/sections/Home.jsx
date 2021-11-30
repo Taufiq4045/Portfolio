0eac0e0f94c5343bdcca1f795cfc23bbba614b45
Taufiq4045 <87647852+Taufiq4045@users.noreply.github.com>
2021-11-30 17:38:48 +0530

    make it better

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -7,6 +7,7 @@ import { AiFillAppstore } from "react-icons/ai";
 import Resume from "../assets/Taufiq.pdf"
 import {Link as LinkS} from 'react-scroll' 
 import { small } from '../responsive';
+import Video from '../assets/Video.mp4';
 
 
 const MainContainer=styled.div`
@@ -26,16 +27,16 @@ const MainDiv=styled.div`
 flex: 1;
 `
 
-const Container=styled.div`
-display: flex;
-padding: 10px;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin-top:9rem;
-text-align: center;
-${small({marginTop:"5rem"})}
-`
+// const Container=styled.div`
+// display: flex;
+// padding: 10px;
+// flex-direction: column;
+// justify-content: center;
+// align-items: center;
+// margin-top:9rem;
+// text-align: center;
+// ${small({marginTop:"5rem"})}
+// `
 
 const HeaderContainer=styled.div`
 color:#0275d8;
@@ -51,6 +52,66 @@ const TContainer=styled.div`
 font-size: 2.5rem;
 font-weight: 700;
 color:#0275d8;
+`
+const VideoBg = styled.video`
+width: 100%;
+height: 100vh/* calc(100vh- 70px) */;
+-o-object-fit: cover;
+object-fit: cover;
+background: #232a34;
+`
+const HomeContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 10px 100px;
+    /* padding-top: 25%;  */
+    display: flex;
+    flex-direction: column;
+    align-items: flex-center; /* flex-start */
+    @media screen and (max-width:370px){
+        padding-top: 25%;
+    }
+`
+const HomeBg = styled.div`
+    position: absolute;
+    top:0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+`
+const HomeContainer = styled.div`
+    background:#0c0c0c ;
+    display: flex;
+    /* justify-content: center; */
+    align-items: center; 
+    margin-top:5rem;
+    padding: 0 30px;
+    height: 100vh/* 608px *//* 800px ==================updated */;  
+    position: relative;
+    z-index: 1;
+    :before{
+        content:'';
+        position: absolute;
+        top:0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: linear-gradient(
+            180deg,
+            rgba(0,0,0,0.2)0%,
+            rgba(0,0,0,0.6)
+        ),
+        linear-gradient(180deg,rgba(0,0,0,0.2)0%,transparent 100%);
+        z-index: 2;
+    }
 `
 
 export default function Home() {
@@ -76,8 +137,12 @@ export default function Home() {
         <div id="home">
         <MainContainer>
 
-            <MainDiv>
-                <Container className="text-justify" >
+            <HomeContainer>
+              <HomeBg>
+              <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+              </HomeBg>
+
+                <HomeContent >
                     <HeaderContainer> TAUFIQ</HeaderContainer>
                     <SContainer ><p >A passionate full stack developer building apps using</p></SContainer>
                     <TContainer>
@@ -92,8 +157,8 @@ export default function Home() {
                         <NavLinks to='projects' spy={true} smooth={true}  duration={500}>Project</NavLinks>
                         </button>
                     </SContainer>
-                </Container>
-        </MainDiv >
+                </HomeContent>
+        </HomeContainer >
 
         <MainDiv  className="d-flex flex-column justify-content-end " >
             <div>
